848573c788ffa7ca5886a5cf6bc4f31fbe26f31b
Rajesh Pattanaik <rkpattanaik21@gmail.com>
2025-07-19 22:29:19 +0530

    feat: setup shadcn/ui and next theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { ThemeToggle } from '@/components/ThemeToggle';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Nirmanam</h1>
+    <div className="flex h-screen flex-col items-center justify-center gap-4">
+      <h1 className="font-bold text-4xl">Nirmanam</h1>
+      <ThemeToggle />
     </div>
   );
 }
